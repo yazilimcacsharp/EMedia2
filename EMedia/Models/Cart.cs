81f02614bfe8131aefa3feb3549dd6204bc39000
aribilgiogr <aribilgiogr@gmail.com>
2026-05-05 21:16:23 +0300

    sepetteki adet bug'ı çözüldü, sepetteki ürünleri gösterme kısmına başladık

---
+++ EMedia/Models/Cart.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EMedia.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int RecordId { get; set; }
+        public string CartId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+        public DateTime DateCreated { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
